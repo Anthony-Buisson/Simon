35c17e1d64d1ad83f5c2583d5b54707cb66ad6fd
Anthony Buisson <anthony.buisson@meliorence.com>
2021-02-11 21:15:33 +0100

    create api

---
+++ back/src/Controller/DefaultController.php
@@ -0,0 +1,61 @@
+<?
+namespace App\Controller;
+
+use App\Entity\Score;
+use App\Repository\ScoreRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+* @Route("/api")
+*/
+class DefaultController extends AbstractController
+{
+    /**
+    * @Route("/scores", name="score_all", methods={"GET"})
+    *
+    * @param ScoreRepository $scoreRepository
+    * @return JsonResponse
+    */
+    public function index(ScoreRepository $scoreRepository): Response {
+        $scores = $scoreRepository->findAll();
+        $arrayCollection = [];
+        foreach($scores as $score) {
+            $arrayCollection[] = array(
+                'pseudo' => $score->getPseudo(),
+                'date' => $score->getDate(),
+                'score' => $score->getScore(),
+            );
+        }
+        return $this->json($arrayCollection,200, ['Access-Control-Allow-Origin' => '*']);
+    }
+
+    /**
+     * @Route("/scores", name="score_new", methods={"POST"})
+     *
+     * @param ScoreRepository $scoreRepository
+     * @param Request $request
+     * @param EntityManagerInterface $em
+     * @return JsonResponse
+     */
+    public function addScore(Request $request, EntityManagerInterface $em): JsonResponse {
+        $content = json_decode($request->getContent());
+        $score = new Score();
+        $score->setDate($content->date);
+        $score->setPseudo($content->pseudo);
+        $score->setScore($content->score);
+        $em->persist($score);
+        $em->flush();
+
+        $resp = array(
+            'pseudo' => $score->getPseudo(),
+            'date' => $score->getDate(),
+            'score' => $score->getScore(),
+        );
+        return $this->json($resp,200, ['Access-Control-Allow-Origin' => '*']);
+    }
+}
---
+++ back/src/Entity/Score.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ScoreRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ScoreRepository::class)
+ */
+class Score
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $score;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $pseudo;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $date;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getScore(): ?int
+    {
+        return $this->score;
+    }
+
+    public function setScore(int $score): self
+    {
+        $this->score = $score;
+
+        return $this;
+    }
+
+    public function getPseudo(): ?string
+    {
+        return $this->pseudo;
+    }
+
+    public function setPseudo(string $pseudo): self
+    {
+        $this->pseudo = $pseudo;
+
+        return $this;
+    }
+
+    public function getDate(): ?string
+    {
+        return $this->date;
+    }
+
+    public function setDate(string $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+}
